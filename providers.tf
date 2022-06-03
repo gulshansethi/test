d712565455768225acbc290431023946acb43fb4
LL\gulsethi <gulshan.sethi@publicissapient.com>
2022-06-03 22:42:33 +0530

    test

--- providers.tf
+++ providers.tf
@@ -13,4 +13,8 @@ terraform {
 provider "azurerm" {
   features {}
   subscription_id = "687dcc1f-cbfe-4498-9116-a769e261d0fd"
+  
+  client_id       = "cnX8Q~kukF4vT9AQTP0iZb24qsFYdhecQ6GEWcS6"
+  client_secret   = "0a066d30-c064-494e-85e8-df8ca7eb2765"
+  tenant_id       = "a6a88f96-f680-48d0-8ada-29dd6889a6fc"
 }
